939d350807666d0d3254afbcc18fc519e1a896ba
Abdul Barik <abdulbarik1997m@gmail.com>
2025-12-30 21:21:49 +0600

    auth

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,21 @@
 import express from "express";
 const app = express();
-import { json } from "body-parser";
 import cors from "cors";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
+
+app.use(
+  cors({
+    origin: process.env.APP_URL || "http://localhost:3000",
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+
+app.all("/api/auth/*splat", toNodeHandler(auth));
+
+// app.use("/posts", postRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello, Prisma Blog!");
