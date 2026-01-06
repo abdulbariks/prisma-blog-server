f35b79139a627b5c96df565523733efd31a4ce17
Abdul Barik <abdulbarik1997m@gmail.com>
2026-01-06 16:24:19 +0600

    Post

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth";
 import { prisma } from "./lib/prisma";
+import { postRouter } from "./modules/post/post.router";
 
 app.use(
   cors({
@@ -16,28 +17,12 @@ app.use(express.json());
 
 app.all("/api/auth/*splat", toNodeHandler(auth));
 
+app.use("/posts", postRouter);
+
 // app.use("/posts", postRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello, Prisma Blog!");
-});
-
-app.post("/blogs", async (req, res) => {
-  try {
-    const { title, content } = req.body;
-
-    if (!title) {
-      return res.status(400).json({ message: "Title is required" });
-    }
-
-    const blog = await prisma.blog.create({
-      data: { title, content },
-    });
-
-    res.json({ success: true, blog });
-  } catch (error: any) {
-    res.status(400).json({ success: false, message: error.message });
-  }
 });
 
 export default app;
